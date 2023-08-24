6fb241e3586db1a6d0171d5000b2c47ea9e055f7
Vinicius <jvinicius_andrade@hotmail.com>
2023-08-24 18:04:43 -0300

    validação da pagina SignUp em java script

--- js/signup.js
+++ js/signup.js
@@ -1,52 +1,125 @@
 // Var do nome inserido
-var nameInput = document.getElementById("name");
-var labelName = document.getElementById("labelName");
+var nameInput = document.getElementById ('name');
+var labelName = document.getElementById ('labelName');
+var validName = false;
 // Var do Email inserido
-var email = document.getElementById("email"); 
-var labelEmail = document.getElementById("labelEmail");
+var email = document.getElementById ('email'); 
+var labelEmail = document.getElementById ('labelEmail');
+var validEmail = false;
 // Var da senha inserida
-var passInput = document.getElementById("passInput");
-var labelPassInput = document.getElementById("labelPassInput");
-// Var do icone da fild senha
-var showPassIcon = document.getElementById("showPassIcon");
+var passInput = document.getElementById ('passInput');
+var labelPassInput = document.getElementById ('labelPassInput');
+var validPass = false;
+// Var do icone da field senha
+var showPassIcon = document.getElementById ('showPassIcon');
 // Var da Senha da field confirmação
-var passInputConfirm = document.getElementById("passInputConfirm");
-var labelpassInputConfirm = document.getElementById("labelpassInputConfirm");
+var passInputConfirm = document.getElementById ('passInputConfirm');
+var labelpassInputConfirm = document.getElementById ('labelpassInputConfirm');
+var validPassConfirm = false;
 // Var icone da field confirmação
-var showPassIconConfirm = document.getElementById("showPassIconConfirm");
+var showPassIconConfirm = document.getElementById ('showPassIconConfirm');
+// var da cor padrão
+var mainColor = "#076001";
+// Var de mensagem de erro e mensagem de sucesso 
+var msgSuccess = document.getElementById ('msgSuccess');
+var msgError = document.getElementById ('msgError');
+// Var do Botão cadastrar e botão login
+var btnSingUp = document.getElementById ('btnSingUp');
+var btnSingIn = document.getElementById ('btnSingIn');
+
 
 // Função para cadastrar Nome
 nameInput.addEventListener('keyup', function() {
   if (nameInput.value.length < 3) {
-    labelName.setAttribute('style', 'color: red');
-    nameInput.setAttribute('style', 'border: 1px solid red; color: red;');
-    nameInput.placeholder = "Insira no minimo 3 caracteres";
+    labelName.setAttribute ('style', 'color: red');
+    nameInput.setAttribute ('style', 'border: 1px solid red');
+    nameInput.placeholder = 'Insira no minimo 3 caracteres';
     labelName.innerHTML = '<i class="fi fi-rs-user">*</i>';
+    validName = false;
   } else {
-    labelName.setAttribute('style', 'color: #076001');
-    nameInput.setAttribute('style', 'border: 1px solid #076001; color: #076001;');
-    nameInput.placeholder = "Nome"; 
-    labelName.innerHTML = '<i class="fi fi-rs-user">';
+    labelName.setAttribute ('style', 'color:' + mainColor);
+    nameInput.setAttribute ('style', 'border: 1px solid' + mainColor);
+    labelName.innerHTML = '<i class="fi fi-rs-user"></i>';
+    validName = true;
   }
 });
 
 // Função para cadastrar E-mail
 email.addEventListener('keyup', function() {
-  if (email.value.length < 3) {
-    labelEmail.setAttribute('style', 'color: red');
-    email.setAttribute('style', 'border: 1px solid red; color: red;');
-    email.placeholder = "Insira um E-mail válido";
+  if (!isValidEmail(email.value)) {
+    labelEmail.setAttribute ('style', 'color: red');
+    email.setAttribute ('style', 'border: 1px solid red');
+    email.placeholder = 'Insira um E-mail válido';
     labelEmail.innerHTML = '<i class="fi fi-rs-envelope">*</i>';
+    validEmail = false;
   } else {
-    labelEmail.setAttribute('style', 'color: #076001');
-    email.setAttribute('style', 'border: 1px solid #076001; color: #076001;');
-    email.placeholder = "Nome";
-    labelEmail.innerHTML = '<i class="fi fi-rs-user">';
+    labelEmail.setAttribute ('style', 'color:' + mainColor);
+    email.setAttribute ('style', 'border: 1px solid' + mainColor);
+    labelEmail.innerHTML = '<i class="fi fi-rs-envelope"></i>';
+    validEmail = true;
+  }
+});
+function isValidEmail(email) {
+  const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  return emailRegex.test(email);
+}
+
+// Função para cadastrar Senha
+passInput.addEventListener('keyup', function() {
+  if (passInput.value.length < 6) {
+    labelPassInput.setAttribute ('style', 'color: red');
+    showPassIcon.setAttribute ('style', 'color: red');
+    passInput.setAttribute ('style', 'border: 1px solid red');
+    passInput.placeholder = 'Insira no mínimo 6 caracteres';
+    labelPassInput.innerHTML = '<i class="fi fi-rr-lock">*</i>';
+    validPass = false;
+  } else {
+    labelPassInput.setAttribute ('style', 'color:' + mainColor);
+    showPassIcon.setAttribute ('style', 'color:' + mainColor);
+    passInput.setAttribute ('style', 'border: 1px solid' + mainColor);
+    labelPassInput.innerHTML = '<i class="fi fi-rr-lock"></i>'
+    validPass = true;
   }
 });
 
+// Função para cadastrar a Confrimação de Senha
+passInputConfirm.addEventListener('keyup', function() {
+  if (passInput.value != passInputConfirm.value) {
+    labelpassInputConfirm.setAttribute ('style', 'color: red');
+    showPassIconConfirm.setAttribute ('style', 'color: red');
+    passInputConfirm.setAttribute ('style', 'border: 1px solid red');
+    passInputConfirm.placeholder = 'Senhas não correspondem';
+    labelpassInputConfirm.innerHTML = '<i class="fi fi-rs-key-skeleton-left-right">*</i>';
+    validPassConfirm = false;
+  } else if (passInputConfirm.value.length < 6 && passInput.value === passInputConfirm.value ) {
+    labelpassInputConfirm.setAttribute ('style', 'color: red');
+    showPassIconConfirm.setAttribute ('style', 'color: red');
+    passInputConfirm.setAttribute ('style', 'border: 1px solid red');
+    passInputConfirm.placeholder = 'Senhas não correspondem';
+    labelpassInputConfirm.innerHTML = '<i class="fi fi-rs-key-skeleton-left-right">*</i>';
+    validPassConfirm = false;
+  } else {
+    labelpassInputConfirm.setAttribute ('style', 'color:' + mainColor);
+    showPassIconConfirm.setAttribute ('style', 'color:' + mainColor);
+    passInputConfirm.setAttribute ('style', 'border: 1px solid' + mainColor);
+    labelpassInputConfirm.innerHTML = '<i class="fi fi-rs-key-skeleton-left-right"></i>'
+    validPassConfirm = true;
+  }
+});
 
+// Função para validar cadastro
 function signUp() {
+  if (validName && validEmail && validPass && validPassConfirm){
+    msgSuccess.setAttribute ('style', 'display: block');
+    msgError.setAttribute ('style', 'display: none');
+    btnSingUp.setAttribute ('style', 'display: none');
+    btnSingIn.setAttribute ('style', 'display: block');
+  } else {
+    msgError.setAttribute ('style', 'display: block');
+    msgSuccess.setAttribute ('style', 'display: none');
+    btnSingUp.setAttribute ('style', 'display: block');
+    btnSingIn.setAttribute ('style', 'display: none');
+  };
 }
 
 // Script mostrar senha
