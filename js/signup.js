076563a80f685e8903d847e10dac3d1d18a9ba4b
Vinicius <jvinicius_andrade@hotmail.com>
2023-08-25 14:27:22 -0300

    validação de cadastro e criação de LocalStorage no JS

--- js/signup.js
+++ js/signup.js
@@ -30,16 +30,18 @@ var btnSingIn = document.getElementById ('btnSingIn');
 
 // Função para cadastrar Nome
 nameInput.addEventListener('keyup', function() {
-  if (nameInput.value.length < 3) {
+  const nameValue = nameInput.value.trim();
+
+  if (nameValue.length < 3 || /^[a-zA-ZÀ-ÿ\sÇç]+$/.test(nameValue) === false) {
     labelName.setAttribute ('style', 'color: red');
-    nameInput.setAttribute ('style', 'border: 1px solid red');
-    nameInput.placeholder = 'Insira no minimo 3 caracteres';
+    nameInput.setAttribute ('style', 'border: 1px solid red; color: red;');
+    nameInput.placeholder = 'Insira no minimo 3 caracteres *';
     labelName.innerHTML = '<i class="fi fi-rs-user">*</i>';
     validName = false;
   } else {
     labelName.setAttribute ('style', 'color:' + mainColor);
     nameInput.setAttribute ('style', 'border: 1px solid' + mainColor);
-    labelName.innerHTML = '<i class="fi fi-rs-user"></i>';
+    labelName.innerHTML = '<i class="fi fi-rr-check"></i>';
     validName = true;
   }
 });
@@ -48,28 +50,28 @@ nameInput.addEventListener('keyup', function() {
 email.addEventListener('keyup', function() {
   if (!isValidEmail(email.value)) {
     labelEmail.setAttribute ('style', 'color: red');
-    email.setAttribute ('style', 'border: 1px solid red');
+    email.setAttribute ('style', 'border: 1px solid red; color: red;');
     email.placeholder = 'Insira um E-mail válido';
     labelEmail.innerHTML = '<i class="fi fi-rs-envelope">*</i>';
     validEmail = false;
   } else {
     labelEmail.setAttribute ('style', 'color:' + mainColor);
     email.setAttribute ('style', 'border: 1px solid' + mainColor);
-    labelEmail.innerHTML = '<i class="fi fi-rs-envelope"></i>';
+    labelEmail.innerHTML = '<i class="fi fi-rr-check"></i>';
     validEmail = true;
   }
 });
 function isValidEmail(email) {
   const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
   return emailRegex.test(email);
-}
+};
 
 // Função para cadastrar Senha
 passInput.addEventListener('keyup', function() {
   if (passInput.value.length < 6) {
     labelPassInput.setAttribute ('style', 'color: red');
     showPassIcon.setAttribute ('style', 'color: red');
-    passInput.setAttribute ('style', 'border: 1px solid red');
+    passInput.setAttribute ('style', 'border: 1px solid red; color: red;');
     passInput.placeholder = 'Insira no mínimo 6 caracteres';
     labelPassInput.innerHTML = '<i class="fi fi-rr-lock">*</i>';
     validPass = false;
@@ -77,32 +79,31 @@ passInput.addEventListener('keyup', function() {
     labelPassInput.setAttribute ('style', 'color:' + mainColor);
     showPassIcon.setAttribute ('style', 'color:' + mainColor);
     passInput.setAttribute ('style', 'border: 1px solid' + mainColor);
-    labelPassInput.innerHTML = '<i class="fi fi-rr-lock"></i>'
+    labelPassInput.innerHTML = '<i class="fi fi-rr-check"></i>'
     validPass = true;
   }
 });
 
-// Função para cadastrar a Confrimação de Senha
+// Função para cadastrar a Confirmação de Senha
 passInputConfirm.addEventListener('keyup', function() {
   if (passInput.value != passInputConfirm.value) {
     labelpassInputConfirm.setAttribute ('style', 'color: red');
     showPassIconConfirm.setAttribute ('style', 'color: red');
-    passInputConfirm.setAttribute ('style', 'border: 1px solid red');
+    passInputConfirm.setAttribute ('style', 'border: 1px solid red; color: red;');
     passInputConfirm.placeholder = 'Senhas não correspondem';
     labelpassInputConfirm.innerHTML = '<i class="fi fi-rs-key-skeleton-left-right">*</i>';
     validPassConfirm = false;
   } else if (passInputConfirm.value.length < 6 && passInput.value === passInputConfirm.value ) {
     labelpassInputConfirm.setAttribute ('style', 'color: red');
     showPassIconConfirm.setAttribute ('style', 'color: red');
-    passInputConfirm.setAttribute ('style', 'border: 1px solid red');
-    passInputConfirm.placeholder = 'Senhas não correspondem';
+    passInputConfirm.setAttribute ('style', 'border: 1px solid red; color: red;');
     labelpassInputConfirm.innerHTML = '<i class="fi fi-rs-key-skeleton-left-right">*</i>';
     validPassConfirm = false;
   } else {
     labelpassInputConfirm.setAttribute ('style', 'color:' + mainColor);
     showPassIconConfirm.setAttribute ('style', 'color:' + mainColor);
     passInputConfirm.setAttribute ('style', 'border: 1px solid' + mainColor);
-    labelpassInputConfirm.innerHTML = '<i class="fi fi-rs-key-skeleton-left-right"></i>'
+    labelpassInputConfirm.innerHTML = '<i class="fi fi-rr-check"></i>';
     validPassConfirm = true;
   }
 });
@@ -110,6 +111,16 @@ passInputConfirm.addEventListener('keyup', function() {
 // Função para validar cadastro
 function signUp() {
   if (validName && validEmail && validPass && validPassConfirm){
+    let userList = JSON.parse (localStorage.getItem ('userList') || '[]');
+
+    userList.push ({
+      nameCad: nameInput.value,
+      emailCad: email.value,
+      passCad: passInput.value,
+    });
+
+    localStorage.setItem ('userList', JSON.stringify(userList));
+
     msgSuccess.setAttribute ('style', 'display: block');
     msgError.setAttribute ('style', 'display: none');
     btnSingUp.setAttribute ('style', 'display: none');
@@ -120,30 +131,35 @@ function signUp() {
     btnSingUp.setAttribute ('style', 'display: block');
     btnSingIn.setAttribute ('style', 'display: none');
   };
-}
+};
+
+// Função para direcionar usuário para página de login
+function signIn() {
+  window.location.href = 'signin.html';
+};
 
 // Script mostrar senha
-showPassIcon.addEventListener("click", function() {
-    if (passInput.type === "password") {
-      passInput.type = "text";
-      showPassIcon.querySelector("i").classList.remove("fi", "fi-rr-eye");
-      showPassIcon.querySelector("i").classList.add("fi", "fi-rr-eye-crossed");
+showPassIcon.addEventListener ('click', function() {
+    if (passInput.type === 'password') {
+      passInput.type = 'text';
+      showPassIcon.querySelector('i').classList.remove('fi', 'fi-rr-eye');
+      showPassIcon.querySelector('i').classList.add('fi', 'fi-rr-eye-crossed');
     } else {
-      passInput.type = "password";
-      showPassIcon.querySelector("i").classList.remove("fi", "fi-rr-eye-crossed");
-      showPassIcon.querySelector("i").classList.add("fi", "fi-rr-eye");
+      passInput.type = 'password';
+      showPassIcon.querySelector('i').classList.remove('fi', 'fi-rr-eye-crossed');
+      showPassIcon.querySelector('i').classList.add('fi', 'fi-rr-eye');
     }
 });
 
 // Script mostrar senha de confirmação
-showPassIconConfirm.addEventListener("click", function() {
-  if (passInputConfirm.type === "password") {
-    passInputConfirm.type = "text";
-    showPassIconConfirm.querySelector("i").classList.remove("fi", "fi-rr-eye");
-    showPassIconConfirm.querySelector("i").classList.add("fi", "fi-rr-eye-crossed");
+showPassIconConfirm.addEventListener ('click', function() {
+  if (passInputConfirm.type === 'password') {
+    passInputConfirm.type = 'text';
+    showPassIconConfirm.querySelector('i').classList.remove('fi', 'fi-rr-eye');
+    showPassIconConfirm.querySelector('i').classList.add('fi', 'fi-rr-eye-crossed');
   } else {
-    passInputConfirm.type = "password";
-    showPassIconConfirm.querySelector("i").classList.remove("fi", "fi-rr-eye-crossed");
-    showPassIconConfirm.querySelector("i").classList.add("fi", "fi-rr-eye");
+    passInputConfirm.type = 'password';
+    showPassIconConfirm.querySelector('i').classList.remove('fi', 'fi-rr-eye-crossed');
+    showPassIconConfirm.querySelector('i').classList.add('fi', 'fi-rr-eye');
   }
 });
